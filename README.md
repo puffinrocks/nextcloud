d19e9274d0ca5360e7b9ae14a943109b8890d1f5
Jarek Lipski <loomchild@loomchild.net>
2017-02-26 03:38:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Nextcloud 
+_a safe home for all your data_
+
+https://nextcloud.com/
+
+Access, share and protect your files, calendars, contacts, communication & more at home and in your enterprise.
+
---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+
+$CONFIG = array(
+
+'dbtype' => 'sqlite',
+
+'mail_domain' => '$DOMAIN',
+'mail_from_address' => 'nextcloud',
+'mail_smtpdebug' => false,
+'mail_smtpmode' => 'smtp',
+'mail_smtphost' => 'mail',
+'mail_smtpport' => 25,
+'mail_smtpsecure' => '',
+'mail_smtpauth' => false,
+
+'overwritehost' => '$DOMAIN',
+
+'singleuser' => true,
+
+'updatechecker' => false,
+
+);
